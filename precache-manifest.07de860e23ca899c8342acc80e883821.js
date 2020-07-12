10de7f0ddf64d71dfa6c8415c28988208ae3d867
HumaidMM <humaid.miakhan@gmail.com>
2020-07-12 23:01:32 +0530

    Updates

---
+++ precache-manifest.07de860e23ca899c8342acc80e883821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59aaf07713e9bf983a00f6c21843ba9",
+    "url": "/jan-ken-pon/index.html"
+  },
+  {
+    "revision": "7996a26910e4801bbc5e",
+    "url": "/jan-ken-pon/static/css/main.0602e604.chunk.css"
+  },
+  {
+    "revision": "33ec6890519b71d05645",
+    "url": "/jan-ken-pon/static/js/2.b6dfed0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jan-ken-pon/static/js/2.b6dfed0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7996a26910e4801bbc5e",
+    "url": "/jan-ken-pon/static/js/main.6bd194af.chunk.js"
+  },
+  {
+    "revision": "c71f0f0d820570ae94c8",
+    "url": "/jan-ken-pon/static/js/runtime-main.7dd8f896.js"
+  }
+]);
